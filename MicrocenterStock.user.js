593936734bb5686cd8a9866db22a4636db6386d0
unknown <38173610+NYCJames@users.noreply.github.com>
2024-01-09 08:02:41 -0500

    Initialize
    Basic userscript template

---
+++ MicrocenterStock.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-09
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.microcenter.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=microcenter.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
